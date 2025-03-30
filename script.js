cc043da84e345e72154963d9816f5f45c780978d
Marco <marco.palummieri@gmail.com>
2025-03-30 15:59:12 +0200

    add svg grafics

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ function render() {
       let index = i * 3 + j;
       let symbol = "";
       if (fields[index] === "circle") {
-        symbol = "O";
+        symbol = generateCircleSVG();
       } else if (fields[index] === "cross") {
-        symbol = "X";
+        symbol = generateCrossSVG();
       }
       tableHTML += `<td>${symbol}</td>`;
     }
@@ -25,4 +25,28 @@ function render() {
 
   tableHTML += "</table>";
   content.innerHTML = tableHTML;
+}
+
+function generateCircleSVG() {
+  return `
+      <svg width="70" height="70" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="50" cy="50" r="40" stroke="#00B0EF" stroke-width="10" fill="none"
+              stroke-dasharray="251.2" stroke-dashoffset="251.2">
+              <animate attributeName="stroke-dashoffset" from="251.2" to="0" dur="0.6s" fill="freeze" />
+          </circle>
+      </svg>
+  `;
+}
+
+function generateCrossSVG() {
+  return `
+      <svg width="70" height="70" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+          <line x1="20" y1="20" x2="80" y2="80" stroke="#FFC000" stroke-width="10" stroke-dasharray="84.85" stroke-dashoffset="84.85">
+              <animate attributeName="stroke-dashoffset" from="84.85" to="0" dur="0.6s" fill="freeze" />
+          </line>
+          <line x1="80" y1="20" x2="20" y2="80" stroke="#FFC000" stroke-width="10" stroke-dasharray="84.85" stroke-dashoffset="84.85">
+              <animate attributeName="stroke-dashoffset" from="84.85" to="0" dur="0.6s" fill="freeze" />
+          </line>
+      </svg>
+  `;
 }
